c6399d2f7262f7ae843dfbdc40f3ce571217c3f4
Rahul Raj <raj_rahul@columbusstate.edu>
2026-06-12 18:27:49 -0400

    added tests for user registration

--- accounts/tests.py
+++ accounts/tests.py
@@ -19,3 +19,52 @@ class RegistrationTestCase(TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get().email, data["email"])
+
+    def test_invalid_email(self):
+        data = {
+            'email': 'testexample',
+            'password': 'StrongPass123!',
+            'first_name': 'John',
+            'last_name': 'Doe'
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 400)
+
+    def test_email_not_provided(self):
+        data = {
+            'password': 'StrongPass123!',
+            'first_name': 'John',
+            'last_name': 'Doe'
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_password_not_provided(self):
+        data = {
+            'email': 'text@example.com',
+            'first_name': 'John',
+            'last_name': 'Doe'
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 400)
+
+    def test_registration_with_duplicate_email(self):
+        User.objects.create_user(email='test@example.com', password='pass')
+        data = {
+            'email': 'test@example.com',
+            'password': 'StrongPass123!',
+            'first_name': 'John',
+            'last_name': 'Doe'
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 400)
+
+    def test_password_not_returned_in_response(self):
+        data = {
+            'email': 'text@example.com',
+            'password': 'StrongPass123!',
+            'first_name': 'John',
+            'last_name': 'Doe'
+        }
+        response = self.client.post(self.url, data)
+        self.assertNotIn('password', response.data)
